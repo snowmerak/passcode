dbbfd306a184634727e9cb8c475ea9fc31c345f8
snowmerak <snowmerak@outlook.com>
2025-10-14 00:42:48 +0900

    feat(blake3): add BLAKE3 keyed mode functions for hashing

---
+++ blake3_keyedmode.go
@@ -0,0 +1,17 @@
+package passcode
+
+import "lukechampine.com/blake3"
+
+func blake3KeyedMode(key []byte, data []byte, outLen int) []byte {
+	hasher := blake3.New(outLen, key)
+	hasher.Write(data)
+	return hasher.Sum(nil)
+}
+
+func BLAKE3KeyedMode256(key, data []byte) []byte {
+	return blake3KeyedMode(key, data, 32)
+}
+
+func BLAKE3KeyedMode512(key, data []byte) []byte {
+	return blake3KeyedMode(key, data, 64)
+}
