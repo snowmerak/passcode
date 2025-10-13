13d2ed0066952aeedae2575fe1cba21b16fbe70a
snowmerak <snowmerak@outlook.com>
2025-10-14 00:49:23 +0900

    refactor(blake3): update keyed mode implementation to use hashed key

--- blake3_keyedmode.go
+++ blake3_keyedmode.go
@@ -1,9 +1,14 @@
 package passcode
 
-import "lukechampine.com/blake3"
+import (
+	"crypto/sha256"
+
+	"lukechampine.com/blake3"
+)
 
 func blake3KeyedMode(key []byte, data []byte, outLen int) []byte {
-	hasher := blake3.New(outLen, key)
+	hasehdKey := blake3.Sum256(key)
+	hasher := blake3.New(outLen, hasehdKey[:])
 	hasher.Write(data)
 	return hasher.Sum(nil)
 }
