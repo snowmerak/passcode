afdf45005f4db44e28504525c531ebf1166027dd
snowmerak <snowmerak@outlook.com>
2025-10-15 01:15:13 +0900

    chore: remove TypeScript implementation and related files for Passcode library

--- ports/typescript/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { Passcode, Algorithm } from './passcode';
-export type { Hasher } from './passcode';
-export { sha3KMAC128, sha3KMAC256 } from './sha3-kmac';
-export { blake3KeyedMode256, blake3KeyedMode512 } from './blake3-keyed';
